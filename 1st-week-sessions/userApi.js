73f10858c794d9cb2456569cac03279e922d3cdd
CRUSADER1912 <saldanha.linus@gmail.com>
2018-01-21 23:26:28 +0530

    Updated userAPI for assignment

--- 1st-week-sessions/userApi.js
+++ 1st-week-sessions/userApi.js
@@ -17,28 +17,43 @@ const profiles = require("./userData");
   hint: google how to find type of data structures in javascript
 */
 
-console.log();
+function isString (value) {
+	return typeof value === 'string' || value instanceof String;
+};
+
+// Returns if a value is an array
+function isArray (value) {
+	return value && typeof value === 'object' && value.constructor === Array;
+};
+
+if(isString(profiles)){
+	console.log("Data is of String type");
+}else if(isArray(profiles)){
+	console.log("Data is of Array type");
+}
 
 /*
   1.2 Log of the size of the data returned
   Expected output: "We just fetched [x] users" where x represents the actual number
 */
 
-console.log();
+console.log('We just fetched '+profiles.length+' users');
 
 /*
   1.3 Get the first five items from the data set
   Expected output: an array with the first 5 items
 */
-console.log();
+console.log(profiles.slice(0,5));
 
 /*
   1.4 Remove the first profile from this data while returning it at the same time
   Expected output: a) user by the name of dânia da mota and b) an array of profiles without dânia da mota 
 */
-
-console.log();
-console.log();
+var firstProfile = profiles.shift();
+console.log('----------------------------------');
+console.log(profiles);
+console.log('----------------------------------');
+console.log(firstProfile); 
 
 /*
   1.5 Paginate the api results in sets of variable amounts
